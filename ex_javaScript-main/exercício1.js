51f99ce09e255c8537a643e540bbee838d0fd09b
Victor de Oliveira <128698672+victor352521@users.noreply.github.com>
2023-11-07 12:10:44 -0300

    Add files via upload

---
+++ ex_javaScript-main/exercício1.js
@@ -0,0 +1,20 @@
+var num1=28;
+var num2=34;
+
+//console.log (num1+num2);
+
+var soma;
+
+function calcularSoma(num1,num2) {
+  soma= num1+num2;
+  return soma;
+}
+
+resultado= calcularSoma(num1, num2);
+console.log('A soma de,'num1',e'num2,é',resultado);
+
+num1=380
+num2=190
+
+resultado= calcularSoma(num1,num2)
+console.log('A soma é:',resultado);
